94684383997ae541323066a43ef8ed89f0975503
Sergei Khoma <srghma@gmail.com>
2018-12-11 22:55:29 +0200

    fix: encode buffer

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = {
   process: (src, filePath) => {
     const mimetype = getType(filePath)
 
-    const content = `data:${mimetype || ''};base64,${btoa(src)}`
+    const src_ = new Buffer(src);
+
+    const content = `data:${mimetype || ''};base64,${src_.toString('base64')}`
 
     return 'module.exports = ' + JSON.stringify(content)
   },
