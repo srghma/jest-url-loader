1c8d04e3bd06b1f872f65083de9ad2fc0674fd93
Sergei Khoma <srghma@gmail.com>
2018-12-11 22:28:46 +0200

    feat: encode

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,4 +1,5 @@
 const { process } = require('../')
+const { base64encode } = require('nodejs-base64');
 
 describe('jest-url-loader', () => {
   it('works as expected', () => {
@@ -6,7 +7,7 @@ describe('jest-url-loader', () => {
     const onePixelBase64 =
       'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='
 
-    const actual = process(atob(onePixelBase64), '/app/pixel.png')
+    const actual = process(base64encode(onePixelBase64), '/app/pixel.png')
 
     expect(actual).toEqual(
       `module.exports = \"data:image/png;base64,${onePixelBase64}\"`
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { getType } = require('mime')
+const { base64decode } = require('nodejs-base64');
 
 module.exports = {
   process: (src, filePath) => {
     const mimetype = getType(filePath)
 
-    const content = `data:${mimetype || ''};base64,${btoa(src)}`
+    const content = `data:${mimetype || ''};base64,${base64decode(src)}`
 
     return 'module.exports = ' + JSON.stringify(content)
   },
