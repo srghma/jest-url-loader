5324feb86f82fce1e6f68b3f3bc12678440cc81f
Sergei Khoma <srghma@gmail.com>
2018-12-11 23:42:00 +0200

    fix: working

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const { getType } = require('mime')
-const { base64encode } = require('nodejs-base64');
+const fs = require('fs');
 
 module.exports = {
-  process: (src, filePath) => {
+  process: (_src, filePath, config) => {
     const mimetype = getType(filePath)
+    const imageAsBase64 = fs.readFileSync(filePath, 'base64');
 
-    const buffer = Buffer.from(src);
-
-    const encoded = buffer.toString('base64')
-
-    const content = `data:${mimetype || ''};base64,${encoded}`
+    const content = `data:${mimetype || ''};base64,${imageAsBase64}`
 
     return 'module.exports = ' + JSON.stringify(content)
   },
