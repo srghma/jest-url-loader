a0becaf6a33fdc0c99130a588f0d32565d53f2a1
Sergei Khoma <srghma@gmail.com>
2018-12-11 22:08:16 +0200

    feat: init

---
+++ __tests__/index.js
@@ -0,0 +1,12 @@
+const { process } = require("../");
+
+describe("jest-url-loader", () => {
+  it("works as expected", () => {
+    // copied from http://png-pixel.com/
+    const onePixelBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=="
+
+    const actual = process(onePixelBase64, '/app/pixel.png');
+
+    expect(actual).toEqual(`module.exports = \"data:image/png;base64,${onePixelBase64}\"`)
+  });
+});
---
+++ index.js
@@ -0,0 +1,11 @@
+const { getType } = require('mime')
+
+module.exports = {
+  process: (src, filePath) => {
+    const mimetype = getType(filePath)
+
+    const content = `data:${mimetype || ''};base64,${src.toString('base64')}`
+
+    return "module.exports = " + JSON.stringify(content)
+  }
+};
