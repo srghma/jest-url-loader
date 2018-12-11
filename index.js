63cfee093497d5b0af6c3342c2bf1128612cea56
Sergei Khoma <srghma@gmail.com>
2018-12-11 22:45:24 +0200

    fix: encode

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   process: (src, filePath) => {
     const mimetype = getType(filePath)
 
+    console.log('src', src)
+
     const content = `data:${mimetype || ''};base64,${base64encode(src)}`
 
     return 'module.exports = ' + JSON.stringify(content)
