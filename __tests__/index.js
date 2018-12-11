45b09a4dc639412c20f8b7c699ffadcf08f71e9a
Sergei Khoma <srghma@gmail.com>
2018-12-11 22:42:50 +0200

    fix: encode

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,16 +1,14 @@
 const { process } = require('../')
-const { base64encode } = require('nodejs-base64');
+const { base64decode, base64encode } = require('nodejs-base64');
 
 describe('jest-url-loader', () => {
   it('works as expected', () => {
     // copied from http://png-pixel.com/
-    const onePixelBase64 =
-      'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=='
+    const content = 'asdfasdf'
 
-    const actual = process(base64encode(onePixelBase64), '/app/pixel.png')
+    const actual = process(content, '/app/pixel.png')
 
-    expect(actual).toEqual(
-      `module.exports = \"data:image/png;base64,${onePixelBase64}\"`
-    )
+    const expected = `module.exports = \"data:image/png;base64,${base64encode(content)}\"`
+    expect(actual).toEqual(expected)
   })
 })
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { getType } = require('mime')
-const { base64decode } = require('nodejs-base64');
+const { base64encode } = require('nodejs-base64');
 
 module.exports = {
   process: (src, filePath) => {
     const mimetype = getType(filePath)
 
-    const content = `data:${mimetype || ''};base64,${base64decode(src)}`
+    const content = `data:${mimetype || ''};base64,${base64encode(src)}`
 
     return 'module.exports = ' + JSON.stringify(content)
   },
