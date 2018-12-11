eedbc62d7d09f863e6f9d4ff58432466d6d09b9a
Sergei Khoma <srghma@gmail.com>
2018-12-11 23:03:40 +0200

    fix: encode buffer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   process: (src, filePath) => {
     const mimetype = getType(filePath)
 
-    const buffer = Buffer.from(str.toString(), 'binary');
+    const buffer = Buffer.from(src.toString(), 'binary');
 
     const encoded = buffer.toString('base64')
 
